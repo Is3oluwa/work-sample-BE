cdd566350bf971776fb4d9b4ed221dee78cf93a6
Iseoluwa <iseoyedotun@gmail.com>
2023-04-05 20:53:10 +0100

    changed cors setup

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
     origin: "*"
-  }});
+  })
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
