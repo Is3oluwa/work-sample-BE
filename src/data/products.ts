522d1a8964671a279a4c6a82af33779a8151e254
Iseoluwa <iseoyedotun@gmail.com>
2023-04-09 14:04:58 +0100

    updated products

--- src/data/products.ts
+++ src/data/products.ts
@@ -32,35 +32,35 @@ export const Products = [
     quantity: 0,
   },
   {
-    name: 'TV',
-    description: 'Best tv in the market',
+    name: 'Mug',
+    description: 'Best mug in the market',
     image:
-      'https://images.unsplash.com/photo-1631117127948-1967f5e724cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTMxfHx0dnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-    price: 1000,
+      'https://images.unsplash.com/photo-1555447014-7ead71574544?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bXVnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    price: 20,
     quantity: 0,
   },
   {
-    name: 'iPhone',
-    description: 'Best iPhone in the market',
+    name: 'Bag',
+    description: 'Best bag in the market',
     image:
-      'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGlwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-    price: 999,
-    quantity: 0,
-  },
-  {
-    name: 'Chromecast',
-    description: 'Best Chromecast in the market',
-    image:
-      'https://images.unsplash.com/photo-1664451659119-7cd033a53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Q2hyb21lY2FzdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+      'https://https://images.unsplash.com/photo-1622560480605-d83c853bc5c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJhY2twYWNrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGlwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
     price: 30,
     quantity: 0,
   },
   {
-    name: 'Glasses',
-    description: 'Best glasses in the market',
+    name: 'Android',
+    description: 'Best android in the market',
     image:
-      'https://images.unsplash.com/photo-1556306510-31ca015374b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdsYXNzZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-    price: 100,
+      'https://images.unsplash.https://images.unsplash.com/photo-1666238851843-c4580232a6a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8UGl4ZWwlMjA3fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60/photo-1664451659119-7cd033a53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Q2hyb21lY2FzdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    price: 799,
+    quantity: 0,
+  },
+  {
+    name: 'Toy',
+    description: 'Best toy in the market',
+    image:
+      'https://images.https://images.unsplash.com/photo-1582845512747-e42001c95638?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dG95fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60unsplash.com/photo-1556306510-31ca015374b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdsYXNzZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    price: 15,
     quantity: 0,
   },
 ];
